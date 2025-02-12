4373a2651401d57f0ef8d52d04ecec5aaf25c45d
Jamison-Lii <ntuvlii5@gmail.com>
2025-02-12 17:51:13 +0100

    latest upload

--- src/Components/Fields/Fields.jsx
+++ src/Components/Fields/Fields.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Fields.css'
 
 const Fields = (prop) => {
   return (
     <div className="Fields">
         
-            <label>{prop.label}</label>
-            <input type={prop.type} name={prop.name} placeholder={prop.placeholder} />
-            <button type="submit">{prop.buttonText}</button>
+            <label id={prop.labelid} for={prop.name}>{prop.label}</label>
+            <input id={prop.id} type={prop.type} name={prop.name} placeholder={prop.placeholder} />
+           {/* <button type="submit">{prop.buttonText}</button>*/}
             
     </div>    
   )
--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import file_img from '../Components/Assets/files-icon.svg'
 import './CSS/Upload.css'
+import Fields from '../Components/Fields/Fields'
 
 const Upload = () => {
   return (
     <div className="upload">
       <img src={file_img} alt="" />
+
+      <Fields className='section-upload' labelid='file-label' id='file' label='Upload' type= 'file' name='file' placeholder='Select File'/>
 
       
     </div>
