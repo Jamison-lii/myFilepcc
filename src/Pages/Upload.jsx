e4a7e92a4dade298730c5970d222ec27d681b50b
Jamison-lii <ntuvlii5@gmail.com>
2025-04-24 07:21:33 +0100

    Update Upload.jsx

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -6,7 +6,7 @@ import Fields from '../Components/Fields/Fields'
 const Upload = () => {
   return (
     <div className="upload">
-      <img src={file_img} alt="" />
+      <img src={file_img} alt="image" />
 
       <Fields className='section-upload' labelid='file-label' id='file' label='Upload' type= 'file' name='file' placeholder='Select File'/>
 
